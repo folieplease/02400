5df8ebdd69dfeb954f2c8211795e8b7da97fd387
Augustine S <folie@wckd.xyz>
2020-09-21 15:54:32 +0000

    Day 1: string functions + methods

---
+++ journal/string.rb
@@ -0,0 +1,14 @@
+# This is a comment
+
+master = "folie"
+
+salutation = "Mr. Grey"
+greetings = "Good morning."
+puts "Hello #{master}, " + greetings
+
+# This is a function
+def MakeABot (first_word)
+  puts first_word
+end
+
+MakeABot "Hi master"
